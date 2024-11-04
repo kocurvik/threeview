28acf04c73db7bdff0e6ec2bc72c7bf07b3a1a43
Viktor Kocur <kocurvik@gmail.com>
2024-11-04 21:20:30 +0100

    vis

--- utils/data.py
+++ utils/data.py
@@ -10,7 +10,7 @@ import seaborn as sns
 experiments = ['4p3v(M) + R + C', '4p3v(M) + R + C + ENM',
                '4p3v(M-D) + R + C', '4p3v(M-D) + R + C + ENM',
                '4p(HC)', '5p3v', '5p3v + ENM', '4p3v(O) + R + C', '4p3v(O) + R + C + ENM',
-               '4p3v(A) + R + C', '3p3v(A)', '2p3v(A)',
+               # '4p3v(A) + R + C', '3p3v(A)', '2p3v(A)',
                '4p3v(A) + R + C + ENM', '3p3v(A) + ENM', '2p3v(A) + ENM']
 
 iterations_list = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
