ddb672993899e1c3e51197ae67ff9a3dd5e5dc4a
Viktor Kocur <kocurvik@gmail.com>
2024-11-04 12:24:36 +0100

    add t optim table

--- utils/tables.py
+++ utils/tables.py
@@ -179,7 +179,11 @@ def generate_delta_table():
 def generate_threshold_table():
     aucs = np.empty([len(experiments), 10])
     for i in range(1, 11):
-        json_path = os.path.join('results', f'st_peters_square-{i:0.1f}-triplets-features_superpoint_noresize_2048-LG.json')
+        if i == 1:
+            json_path = os.path.join('results',
+                                     f'st_peters_square-triplets-features_superpoint_noresize_2048-LG.json')
+        else:
+            json_path = os.path.join('results', f'st_peters_square-{i:0.1f}-triplets-features_superpoint_noresize_2048-LG.json')
         print(f'json_path: {json_path}')
         with open(json_path, 'r') as f:
             results = ([x for x in json.load(f) if 'D(' in x['experiment']])
